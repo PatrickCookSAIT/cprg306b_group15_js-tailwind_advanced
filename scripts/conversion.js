e932d90c6ea8dbd3cc5485a89866c60030f53e17
PatrickCookSAIT <patrick.cook@edu.sait.ca>
2026-05-29 10:45:30 -0600

    swapping units

--- scripts/conversion.js
+++ scripts/conversion.js
@@ -1,5 +1,6 @@
 console.log("JS loaded");
 
+
 let unitConversionMultiplier = 0.621371;
 
 function convert() {
@@ -12,4 +13,17 @@ function convert() {
     resultID.innerHTML = result;
 }
 
+function swapUnits() {
+    const userUnitID = document.getElementById("userUnitID");
+    const resultUnitID = document.getElementById("resultUnitID");
+
+    //the swap happens here by taking unit currently in the results portion and moving it to user portion
+    const userUnit = resultUnitID.innerHTML;
+    const resultUnit = userUnitID.innerHTML;
+
+    //plug new results back in
+    resultUnitID.innerHTML = resultUnit;
+    userUnitID.innerHTML = userUnit;
+}
 window.convert = convert;
+window.swapUnits - swapUnits;
